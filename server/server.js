0102b7c10a0f08227eec4933a2e621ccee7d4c32
ayushjha119 <ayush.jha.it23@heritageit.edu.in>
2023-11-23 22:36:15 +0530

    Google Auth added

--- server/server.js
+++ server/server.js
@@ -6,9 +6,16 @@ import { nanoid } from "nanoid";
 import jwt from "jsonwebtoken";
 import "dotenv/config";
 import cors from "cors";
+import admin from "firebase-admin";
+import serviceAccountKey from "./geeksblogging2-firebase-adminsdk-zi2dt-e791a9d522.json" assert { type: "json" };
+import { getAuth } from "firebase-admin/auth";
 
 const server = express();
 let PORT = 3000;
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccountKey),
+});
 
 let emailRegex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/; // regex for email
 let passwordRegex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,20}$/; // regex for password
@@ -95,22 +102,87 @@ server.post("/signin", (req, res) => {
       if (!user) {
         return res.status(403).json({ error: "Email does not exist" });
       }
-      bcrypt.compare(password, user.personal_info.password, (err, result) => {
-        if (err) {
-          return res
-            .status(403)
-            .json({ error: "Error occurred while login please try again" });
-        }
-        if (!result) {
-          return res.status(403).json({ error: "Password is incorrect" });
-        } else {
-          return res.status(200).json(formatDatatoSend(user));
-        }
-      });
+
+      if (!user.google_auth) {
+        bcrypt.compare(password, user.personal_info.password, (err, result) => {
+          if (err) {
+            return res
+              .status(403)
+              .json({ error: "Error occurred while login please try again" });
+          }
+          if (!result) {
+            return res.status(403).json({ error: "Password is incorrect" });
+          } else {
+            return res.status(200).json(formatDatatoSend(user));
+          }
+        });
+      } else {
+        return res
+          .status(403)
+          .json({
+            error:
+              "Email already exists log in with google to access the account",
+          });
+      }
     })
     .catch((err) => {
       console.log(err);
       return res.status(500).json({ error: err.message });
+    });
+});
+
+server.post("/google-auth", async (req, res) => {
+  let { access_token } = req.body;
+  getAuth()
+    .verifyIdToken(access_token)
+    .then(async (decodedUser) => {
+      let { email, name, picture } = decodedUser;
+      picture = picture.replace("s96-c", "s384-c");
+      let user = await User.findOne({ "personal_info.email": email })
+        .select(
+          "personal_info.fullname personal_info.username personal_info.profile_img google_auth"
+        )
+        .then((u) => {
+          return u || null;
+        })
+        .catch((err) => {
+          return res.status(500).json({ error: err.message });
+        });
+      if (user) {
+        if (!user.google_auth) {
+          return res.status(403).json({
+            error:
+              "Email already exists log in with password to access the account",
+          });
+        }
+      } else {
+        let username = await generateUsername(email);
+        user = new User({
+          personal_info: {
+            fullname: name,
+            email,
+            username,
+            profile_img: picture,
+            // u can remove this to generate random image
+            // just one line 155
+          },
+          google_auth: true,
+        });
+        await user
+          .save()
+          .then((u) => {
+            user = u;
+          })
+          .catch((err) => {
+            return res.status(500).json({ error: err.message });
+          });
+      }
+      return res.status(200).json(formatDatatoSend(user));
+    })
+    .catch((err) => {
+      return res
+        .status(500)
+        .json({ error: "Failed to authenticate you with google." });
     });
 });
 
