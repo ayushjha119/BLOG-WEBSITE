db6c99231599f1a4b76656df7055f4f7baddfd62
ayushjha119 <ayush.jha.it23@heritageit.edu.in>
2023-11-24 17:38:22 +0530

    NavBar setup for blog editor

---
+++ blogging website - frontend/src/components/blog-editor.component.jsx
@@ -0,0 +1,21 @@
+import { Link } from "react-router-dom";
+import logo from "../imgs/logo.png";
+
+const BlogEditor = () => {
+  return (
+    <>
+      <nav className="navbar">
+        <Link to="/" className="flex-none w-10 ">
+          <img src={logo} />
+        </Link>
+        <p className="max-md:hidden text-black line-clamp-1 w-full">New Blog</p>
+        <div className="flex gap-4 ml-auto">
+          <button className="btn-dark py-2">publish</button>
+          <button className="btn-light py-2">Save draft</button>
+        </div>
+      </nav>
+    </>
+  );
+};
+
+export default BlogEditor;
