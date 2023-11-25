f8458bd09f013b6e3d9b5f83509614425845b092
ayushjha119 <ayush.jha.it23@heritageit.edu.in>
2023-11-25 15:07:17 +0530

    Blog Editior done

--- blogging website - frontend/src/components/blog-editor.component.jsx
+++ blogging website - frontend/src/components/blog-editor.component.jsx
@@ -3,9 +3,11 @@ import logo from "../imgs/logo.png";
 import AnimationWrapper from "../common/page-animation";
 import defaultBanner from "../imgs/blog banner.png";
 import { uploadImage } from "../common/aws";
-import { useContext } from "react";
+import { useContext, useEffect } from "react";
 import { Toaster, toast } from "react-hot-toast";
-import { EditorContext } from "../pages/editor.pages";
+import Editor, { EditorContext } from "../pages/editor.pages";
+import EditorJS from "@editorjs/editorjs";
+import { tools } from "./tools.component";
 
 const BlogEditor = () => {
   //let blogBannerRef = useRef();
@@ -15,6 +17,17 @@ const BlogEditor = () => {
     blog: { title, banner, content, tags, des },
     setBlog,
   } = useContext(EditorContext);
+
+  //useEffect hook
+
+  useEffect(() => {
+    let editor = new EditorJS({
+      holder: "textEditor",
+      data: "",
+      tools: tools,
+      placeholder: "Start writing your blog...",
+    });
+  }, []);
 
   const handleBannerUpload = (e) => {
     let img = e.target.files[0];
@@ -94,6 +107,8 @@ const BlogEditor = () => {
             ></textarea>
 
             <hr className="w-full opacity-10 my-5" />
+
+            <div id="textEditor" className="font-gelasio"></div>
           </div>
         </section>
       </AnimationWrapper>
