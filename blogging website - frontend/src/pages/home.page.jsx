3ade81f74f4d1e4ca3fcae36f0c5aff915d4a3ca
ayushjha119 <ayush.jha.it23@heritageit.edu.in>
2023-12-01 19:37:54 +0530

    inPageNavigation Done

--- blogging website - frontend/src/pages/home.page.jsx
+++ blogging website - frontend/src/pages/home.page.jsx
@@ -1,8 +1,23 @@
+import AnimationWrapper from "../common/page-animation";
+import InPageNavigation from "../components/inpage-navigation.component";
 const HomePage = () => {
   return (
-    <div>
-      <h1>Home Page from component</h1>
-    </div>
+    <AnimationWrapper>
+      <section className="h-cover flex justify-center gap-10">
+        {/* latest blog */}
+        <div className="w-full">
+          <InPageNavigation
+            routes={["home", "trending blogs"]}
+            defaultHidden={["trending blogs"]}
+          >
+            <h1>Latest Blogs Here</h1>
+            <h1>Trending Blogs Here</h1>
+          </InPageNavigation>
+        </div>
+        {/* filter treanding blog */}
+        <div></div>
+      </section>
+    </AnimationWrapper>
   );
 };
 
