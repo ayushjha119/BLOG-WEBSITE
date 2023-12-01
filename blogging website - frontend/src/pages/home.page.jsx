05bc709ca2b711974b377ddcf307ecd4a20f87a8
ayushjha119 <ayush.jha.it23@heritageit.edu.in>
2023-12-01 20:44:15 +0530

    fetching latest blogs

--- blogging website - frontend/src/pages/home.page.jsx
+++ blogging website - frontend/src/pages/home.page.jsx
@@ -1,6 +1,27 @@
+import { useEffect, useState } from "react";
+import Loader from "../components/loader.component";
 import AnimationWrapper from "../common/page-animation";
 import InPageNavigation from "../components/inpage-navigation.component";
+import axios from "axios";
 const HomePage = () => {
+  let [blogs, setBlog] = useState(null);
+
+  const fetchLatestBlogs = () => {
+    // fetch latest blogs
+    axios
+      .get(import.meta.env.VITE_SERVER_DOMAIN + "/latest-blogs")
+      .then(({ data }) => {
+        setBlog(data.blogs);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  useEffect(() => {
+    fetchLatestBlogs();
+  }, []);
+
   return (
     <AnimationWrapper>
       <section className="h-cover flex justify-center gap-10">
@@ -10,7 +31,15 @@ const HomePage = () => {
             routes={["home", "trending blogs"]}
             defaultHidden={["trending blogs"]}
           >
-            <h1>Latest Blogs Here</h1>
+            <>
+              {blogs == null ? (
+                <Loader />
+              ) : (
+                blogs.map((blog, i) => {
+                  return <h1 key={i}>{blog.title}</h1>;
+                })
+              )}
+            </>
             <h1>Trending Blogs Here</h1>
           </InPageNavigation>
         </div>
