618fa9b0a613434c34f185fa0c43454fbba60da1
ayushjha119 <ayush.jha.it23@heritageit.edu.in>
2023-12-08 23:21:18 +0530

    Like blog part done

--- blogging website - frontend/src/components/blog-interaction.component.jsx
+++ blogging website - frontend/src/components/blog-interaction.component.jsx
@@ -1,31 +1,91 @@
-import { useContext } from "react";
+import { useContext, useEffect } from "react";
 import { BlogContext } from "../pages/blog.page";
 import { Link } from "react-router-dom";
 import { UserContext } from "../App";
+import { Toaster, toast } from "react-hot-toast";
+import axios from "axios";
 const BlogInteraction = () => {
   let {
+    blog,
     blog: {
+      _id,
       title,
       blog_id,
+      activity,
       activity: { total_likes, total_comments },
       author: {
         personal_info: { username: author_username },
       },
     },
     setBlog,
+    islikedByUser,
+    setIsLikedByUser,
   } = useContext(BlogContext);
 
   let {
-    userAuth: { username },
+    userAuth: { username, access_token },
   } = useContext(UserContext);
+
+  useEffect(() => {
+    if (access_token) {
+      axios
+        .post(
+          import.meta.env.VITE_SERVER_DOMAIN + "/isliked-by-user",
+          { _id },
+          { headers: { Authorization: `Bearer ${access_token}` } }
+        )
+        .then(({ data: { result } }) => {
+          setIsLikedByUser(Boolean(result));
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    }
+  }, []);
+
+  const handleLike = () => {
+    if (access_token) {
+      setIsLikedByUser((preVal) => !preVal);
+      !islikedByUser ? total_likes++ : total_likes--;
+      setBlog({ ...blog, activity: { ...activity, total_likes } });
+      axios
+        .post(
+          import.meta.env.VITE_SERVER_DOMAIN + "/like-blog",
+          {
+            _id,
+            islikedByUser,
+          },
+          { headers: { Authorization: `Bearer ${access_token}` } }
+        )
+        .then(({ data }) => {
+          console.log(data);
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    } else {
+      toast.error("You need to login first to like this blog");
+    }
+  };
 
   return (
     <>
+      <Toaster />
       <hr className="border-grey my-2" />
       <div className="flex gap-6 justify-between">
         <div className="flex gap-3 items-center">
-          <button className="w-10 h-10 rounded-full flex items-center justify-center bg-grey/80">
-            <i className="fi fi-rr-heart"></i>
+          <button
+            onClick={handleLike}
+            className={
+              "w-10 h-10 rounded-full flex items-center justify-center " +
+              (islikedByUser ? "bg-red/20 text-red" : "bg-grey/80")
+            }
+          >
+            <i
+              className={
+                "fi " + (islikedByUser ? "fi-sr-heart" : "fi-rr-heart")
+              }
+            ></i>
           </button>
           <p className="text-xl text-dark-grey">{total_likes}</p>
 
--- server/server.js
+++ server/server.js
@@ -11,6 +11,7 @@ import admin from "firebase-admin";
 import serviceAccountKey from "./geeksblogging2-firebase-adminsdk-zi2dt-e791a9d522.json" assert { type: "json" };
 import { getAuth } from "firebase-admin/auth";
 import aws from "aws-sdk";
+import Notification from "./Schema/Notification.js";
 
 const server = express();
 let PORT = 3000;
@@ -465,6 +466,52 @@ server.post("/get-blog", (req, res) => {
       }
 
       return res.status(200).json({ blog });
+    })
+    .catch((err) => {
+      return res.status(500).json({ error: err.message });
+    });
+});
+
+server.post("/like-blog", verifyJWT, (req, res) => {
+  let user_id = req.user;
+  let { _id, islikedByUser } = req.body;
+  let incrementVal = !islikedByUser ? 1 : -1;
+  Blog.findOneAndUpdate(
+    { _id },
+    { $inc: { "activity.total_likes": incrementVal } }
+  ).then((blog) => {
+    if (!islikedByUser) {
+      let like = new Notification({
+        type: "like",
+        blog: _id,
+        notification_for: blog.author,
+        user: user_id,
+      });
+      like.save().then((notification) => {
+        return res.status(200).json({ liked_by_user: true });
+      });
+    } else {
+      Notification.findOneAndDelete({
+        user: user_id,
+        type: "like",
+        blog: _id,
+      })
+        .then((data) => {
+          return res.status(200).json({ liked_by_user: false });
+        })
+        .catch((err) => {
+          return res.status(500).json({ error: err.message });
+        });
+    }
+  });
+});
+
+server.post("/isliked-by-user", verifyJWT, (req, res) => {
+  let user_id = req.user;
+  let { _id } = req.body;
+  Notification.exists({ user: user_id, type: "like", blog: _id })
+    .then((result) => {
+      return res.status(200).json({ result });
     })
     .catch((err) => {
       return res.status(500).json({ error: err.message });
